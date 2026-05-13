99df802219d54c6963649fc61318f24a9fcc4ff4
Hasnain Raza <chandiohasnain807@gmail.com>
2026-05-13 20:49:59 +0500

    Service Final Commit.

--- src/ui/AppWrapper.java
+++
@@ -1,9 +0,0 @@
-package ui;
-
-public class AppWrapper {
-    public static void main(String[] args) {
-        // This wrapper circumvents the "JavaFX runtime components are missing" error
-        // when running JavaFX 11+ applications without modules or VM options.
-        Main.main(args);
-    }
-}
